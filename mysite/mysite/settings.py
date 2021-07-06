29a703e4e44ed66417693b088c4dfe19b0c94fd2
Sean <yh712@scarletmail.rutgers.edu>
2021-07-06 13:45:50 -0400

    Allow host: pythonanywhere

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = '=@mgxkqw74!kb6e%jevk64!2=j%a%g_0m%*ky^n5#(ei*oe4cw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["yh712.pythonanywhere.com"]
 
 # Application definition
 
